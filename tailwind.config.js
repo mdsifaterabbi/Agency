501235e153e9f5616fb4808f99ab743cc7db6ad0
Md Sifat E Rabbi <mdsifaterabbi641@gmail.com>
2024-02-09 02:01:09 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
+};
